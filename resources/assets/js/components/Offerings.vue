f69cebe51453307f056416abfc2eb2cdc9491ef4
Oleksandr Zaitsev <olexandr.zaitsev.92@gmail.com>
2017-04-11 19:25:18 +0300

    finish all work

--- resources/assets/js/components/Offerings.vue
+++ resources/assets/js/components/Offerings.vue
@@ -6,21 +6,21 @@
                 There are no offerings, yet :(
             </template>
             <template v-else>
-                <form @onsubmit.prevent="save()">
+                <form @submit.prevent="save()">
                     <div class="form-group">
                         <label for="customer_name">Customer Name:</label>
-                        <input type="text" class="form-control" id="customer_name">
+                        <input v-model.trim="customer_name" placeholder="Enter customer name..." type="text" class="form-control" id="customer_name" required>
                     </div>
                     <div class="form-group">
                         <label for="quantity">Quantity:</label>
-                        <input type="text" class="form-control" id="quantity">
+                        <input @change="castToInt" v-model.number="quantity" placeholder="Enter quantity..." type="number" class="form-control" id="quantity" required>
                     </div>
                     <div class="form-group">
                         <label for="offering_id">Quantity:</label>
-                        <select id="offering_id">
-                            <!--<option v-for="offering as offerings">-->
-                                <!--{{ offering.title }} {{ offering.price }}$-->
-                            <!--</option>-->
+                        <select v-model="offering_id" id="offering_id" class="form-control" required>
+                            <option v-for="element in offerings" v-bind:value="element.id">
+                                {{ element.title }} {{ element.price }}$
+                            </option>
                         </select>
                     </div>
                     <button type="submit" class="btn btn-default">Submit</button>
@@ -32,10 +32,16 @@
 
 <script>
     import axios from 'axios';
+    import swal from 'sweetalert'
+    import _ from 'lodash'
 
     export default {
         data() {
             return {
+                offering_id: null,
+                customer_name: null,
+                price: 0,
+                quantity: 0,
                 offerings: []
             }
         },
@@ -43,19 +49,46 @@
             this.fetch();
         },
         methods: {
-            fetch() {
-                let self = this;
+            castToInt() {
+                let parsed = parseInt(this.quantity);
 
+                if (isNaN(parsed)) {
+                    this.quantity =  0;
+                } else {
+                    this.quantity = parsed;
+                }
+            },
+            fetch() {
                 axios.get('/api/offerings')
-                    .then(function (response) {
-                        self.offerings = response.data
+                    .then(response => {
+                        this.offerings = response.data
                     })
-                    .catch(function (error) {
-                        alert(error);
+                    .catch(() => {
+                        swal("Oops...", "Something went wrong!", "error");
                     });
             },
             save() {
+                if (!this.customer_name || !this.offering_id || !this.quantity) {
+                    swal("Oops...", "Fill all fields, please!", "error");
+                }
 
+                if (this.quantity < 1) {
+                    swal("Oops...", "Quantity mast be greater than 0!", "error");
+                }
+
+                let payload = {
+                    customer_name: this.customer_name,
+                    offering_id: this.offering_id,
+                    quantity: this.quantity
+                };
+
+                axios.post('/api/purchases', payload)
+                    .then(() => {
+                        swal("Hooray!", "Purchase have been successfully created!", "success");
+                    })
+                    .catch(()  => {
+                        swal("Oops...", "Something went wrong!", "error");
+                    });
             }
         }
     }
--- resources/assets/js/components/Purchases.vue
+++ resources/assets/js/components/Purchases.vue
@@ -3,8 +3,31 @@
         <h1>Purchases</h1>
         <div>
             <template v-if="!purchases.length">
-                asdasd
+                There are no purchase, yet :(
             </template>
+            <template v-else>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Purchase ID</th>
+                            <th>Offering title</th>
+                            <th>Quantity</th>
+                            <th>Unit price</th>
+                            <th>Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="purchase in purchases">
+                            <td>{{ purchase.id }}</td>
+                            <td>{{ purchase.offering.title }}</td>
+                            <td>{{ purchase.quantity }}</td>
+                            <td>{{ purchase.offering.price }}$</td>
+                            <td>{{ purchase.total_price }}$</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </template>
+
         </div>
     </div>
 </template>
@@ -17,11 +40,17 @@
             }
         },
         mounted() {
-            console.log('Component mounted.')
+           this.fetch();
         },
         methods: {
             fetch() {
-
+                axios.get('/api/purchases')
+                    .then(response => {
+                        this.purchases = response.data
+                    })
+                    .catch(() => {
+                        swal("Oops...", "Something went wrong!", "error");
+                    });
             }
         }
     }
